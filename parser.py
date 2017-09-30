0bef25088764a6b6e9ad962adbe1e5d61fc2b6a3
Huaying Tsai <royal3501@gmail.com>
2017-09-30 03:40:06 -0700

    support python2

--- parser.py
+++ parser.py
@@ -1,17 +1,18 @@
+# coding:utf-8
 import re
 
 
 city = [
-    '台北市', '新北市', '基隆市','宜蘭縣',
-    '新竹市', '新竹縣', '桃園市','苗栗縣',
-    '台中市', '彰化縣', '南投縣','雲林縣',
-    '嘉義市', '嘉義縣', '台南市','高雄市',
-    '屏東縣', '台東縣', '花蓮縣','澎湖縣',
-    '金門縣', '連江縣'
+    u'台北市', u'新北市', u'基隆市', u'宜蘭縣',
+    u'新竹市', u'新竹縣', u'桃園市', u'苗栗縣',
+    u'台中市', u'彰化縣', u'南投縣', u'雲林縣',
+    u'嘉義市', u'嘉義縣', u'台南市', u'高雄市',
+    u'屏東縣', u'台東縣', u'花蓮縣', u'澎湖縣',
+    u'金門縣', u'連江縣'
 ]
 other_city = [
-    '台北縣', '高雄縣', '臺北市', '臺北縣',
-    '台中縣', '臺中市', '臺中縣'
+    u'台北縣', u'高雄縣', u'臺北市', u'臺北縣',
+    u'台中縣', u'臺中市', u'臺中縣'
 ]
 
 
@@ -20,7 +21,7 @@ def parse(text):
         input: text(str)
         output: a list of address(array)
     '''
-    addr_main = '[%s]\w+?[路|街]\w*?\d{1,5}號' % '|'.join(city + other_city)
-    addr_optional = '(?:\d{1,3}樓)?(?:\d{1,3}室)?(?:[\-|之]\d{1,2})?'
+    addr_main = u'[%s]\w+?[路|街]\w*?\d{1,5}號' % '|'.join(city + other_city)
+    addr_optional = u'(?:\d{1,3}樓)?(?:\d{1,3}室)?(?:[\-|之]\d{1,2})?'
     pattern = addr_main + addr_optional
-    return re.findall(pattern, text)
+    return re.findall(pattern, text, re.UNICODE)
